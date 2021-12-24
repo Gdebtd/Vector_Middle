f3d7ac94e6be4a4a3f539123cadf274f408d3d0f
Gdebtd <76738374+Gdebtd@users.noreply.github.com>
2021-12-24 17:12:52 +0300

    middle_list

--- middle_list.h
+++ middle_list.h
@@ -7,8 +7,28 @@
 
 using namespace std;
 
+// additional
 int len(string str);
+bool is_even(int num);
 
+int cnt_even(const vector <int> & lst);
+int cnt_odd(const vector <int> & lst);
+int max_even(const vector <int> & lst);
+int min_even(const vector <int> & lst);
+int max_odd(const vector <int> & lst);
+int min_odd(const vector <int> & lst);
+
+int cnt_pos(const vector <int> & lst);
+int cnt_neg(const vector <int> & lst);
+int cnt_zero(const vector <int> & lst);
+int sum_pos(const vector <int> & lst);
+int sum_neg(const vector <int> & lst);
+int max_pos(const vector <int> & lst);
+int min_pos(const vector <int> & lst);
+int max_neg(const vector <int> & lst);
+int min_neg(const vector <int> & lst);
+
+// main functions
 vector <char> itc_strlist(string str);
 string itc_join(vector <char> lst, string sep);
 string itc_rmstrspc(string str);
@@ -21,5 +41,6 @@ void itc_odd_even_separator_lst(const vector <int> & lst, vector <int> & lst1, v
 void itc_pos_neg_separator_lst(const vector <int> & lst, vector <int> & lst1, vector <int> & lst2, vector <int> & lst3);
 
 void itc_odd_even_analysis_lst(const vector <int> & lst);
+void itc_pos_neg_analysis_lst(const vector <int> & lst);
 
 #endif
